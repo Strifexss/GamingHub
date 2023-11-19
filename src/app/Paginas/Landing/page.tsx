e6d4077e19975dde30b1ad4b18404bbde32da659
Strifexss <rocombole0007@gmail.com>
2023-11-19 10:51:48 -0300

    Correcoes para vercel

--- src/app/Paginas/Landing/page.tsx
+++ src/app/Paginas/Landing/page.tsx
@@ -12,12 +12,8 @@ export default function Landing() {
 
     const {GetCookies} = useContext(AuthContext) as CreateContextProps
     
-    const UserData = GetCookies()
+    const UserData = GetCookies?.()
 
-    useEffect(() => {
-        console.log(UserData)
-    } 
-    ,[])
 
     return(
         <div className="w-screen h-screen flex flex-col items-center bg-zinc-950 py-4 px-10 text-[0.8rem] md:text-[0.6rem] 2xl:text-[0.8rem] overflow-y-scroll gap-y-6">
